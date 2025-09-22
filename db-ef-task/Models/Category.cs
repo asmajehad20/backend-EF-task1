12c7c97805bdb096bf0daa9c5bc8721ee21f135e
asma jehad <126648311+asmajehad20@users.noreply.github.com>
2025-09-22 17:59:24 +0300

    Add files via upload

---
+++ db-ef-task/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_ef_task.Models
+{
+    internal class Category
+    {
+        [Key]
+        public int CatId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
