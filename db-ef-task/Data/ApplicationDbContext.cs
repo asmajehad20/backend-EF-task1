51f6cf7fda2373b8f70d576e8b19c3ab4f18a1f9
asma jehad <126648311+asmajehad20@users.noreply.github.com>
2025-09-22 18:00:44 +0300

    Add files via upload

---
+++ db-ef-task/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using db_ef_task.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db_ef_task.Data
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-PBMMBBC; Database=EF_task; TrustServerCertificate=True; Trusted_connection=True;");
+        }
+    }
+}
